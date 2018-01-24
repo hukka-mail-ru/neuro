7a621144173ca684a3bc29e22e15dd7584d4b25f
hukka-mail.ru <hukka@mail.ru>
2018-01-24 15:39:53 +0300

    dfdf

--- src/neuro.py
+++ src/neuro.py
@@ -4,9 +4,6 @@ import math
 def sigmoid(x: float):
     return 1.0 / (1.0 + math.exp(-x))
 
-def error(expected: float, actual: float, tries: int):
-    return (expected - actual)**2 /  tries
-
 w1=0.45
 w2=0.78 
 w3=-0.12 
@@ -14,8 +11,8 @@ w4=0.13
 w5=1.5 
 w6=-2.3
 
-E = 0.7
-A = 0.3
+E = 0.5
+A = 0
 
 
 ''' 1  '''
@@ -28,6 +25,7 @@ olddw1 = 0
 olddw2 = 0 
 olddw3 = 0 
 olddw4 = 0 
+sumKv = 0
 
 for x in range(1, 100):
 
@@ -42,50 +40,50 @@ for x in range(1, 100):
     
     print("O1", O1)
     
-    e = error(expectedO1, O1, 1)
+    ''' error '''
     
+    sumKv += (expectedO1 - O1)**2
+    e = sumKv  / x
     print("e", e)
+    print("------------")
     
     
     ''' backpropagate '''
     
-    deltaO1 = (expectedO1 - O1) * (1 - O1) * O1
+    dO1 = (expectedO1 - O1) * (1 - O1) * O1
     
-    deltaH1 = (1 - H1) * H1 * (w5 * deltaO1)
-    GRADw5 = H1 * deltaO1
+    dH1 = (1 - H1) * H1 * (w5 * dO1)
+    GRADw5 = H1 * dO1
     dw5 = E * GRADw5 + A * 0
     w5 += dw5
     
     
-    deltaH2 = (1 - H2) * H2 * (w6 * deltaO1)
-    GRADw6 = H2 * deltaO1
+    dH2 = (1 - H2) * H2 * (w6 * dO1)
+    GRADw6 = H2 * dO1
     dw6 = E * GRADw6 + A * 0
     w6 += dw6
     
     
-    deltaI2 = (1 - I2) * I2 * (w1 * deltaH1 + w2 * deltaH2)
-    
-    GRADw1 = I1 * deltaH1
+   
+    GRADw1 = I1 * dH1
     dw1 = E * GRADw1 + A * 0
     olddw1 = dw1
     w1 += dw1
     
-    GRADw2 = I1 * deltaH2
+    GRADw2 = I1 * dH2
     dw2 = E * GRADw2 + A * 0
     olddw2 = dw2
     w2 += dw2
     
-    GRADw3 = I2 * deltaH1
+    GRADw3 = I2 * dH1
     dw3 = E * GRADw3 + A * 0
     olddw3 = dw3
     w3 += dw3
     
-    GRADw4 = I2 * deltaH2
+    GRADw4 = I2 * dH2
     dw4 = E * GRADw4 + A * 0
     olddw4 = dw4
     w4 += dw4
-
-
 
 
 
