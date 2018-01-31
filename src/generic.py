c092c93421783a57d58a9ef009d110bf9a976260
hukka-mail.ru <hukka@mail.ru>
2018-01-31 14:53:36 +0300

    d

--- src/generic.py
+++ src/generic.py
@@ -89,12 +89,73 @@ class Neuron():
         for s in self.outSinapses:
             print("out: ", s.getWeight())
  
-    def getOut(self):
+    def calcValue(self):
         net = self.b
         for sinaps in self.inSinapses:
             net += sinaps.getWeight() * sinaps.getStartNeuron().getValue()    
         self.value = sigmoid(net)
-        return self.value
+ 
+iNeurons = [] 
+hNeurons = [] 
+oNeurons = []  
+
+
+
+def learn(i1v: float, i2v: float, target_o1: float, target_o2: float):
+    
+
+    
+    #i1.setValue(i1v)
+    #i2.setValue(i2v)
+    
+    for h in hNeurons: 
+        h.calcValue()
+
+    for o in oNeurons: 
+        o.calcValue()
+    
+    Etotal = 0
+    for o in oNeurons:
+        Etotal += 1/2 * (o.getTarget() - o.getValue())**2
+      
+    print("learn ", i1v, i2v, "->", target_o1, target_o2)  
+    print("----------------")
+    print("out_o1 %.2f" % out_o1)
+    print("out_o2 %.2f" % out_o2)
+    print("----------------")    
+    print("Etotal %.8f" % Etotal)
+    
+        
+    # backpropagate 
+    
+    delta_o1 = (out_o1 - target_o1) * out_o1 * (1 - out_o1)
+    delta_o2 = (out_o2 - target_o2) * out_o2 * (1 - out_o2)
+       
+    dEtotal_dout_h1 = delta_o1 * w5.getWeight() + delta_o2 * w7.getWeight()
+    dEtotal_dout_h2 = delta_o1 * w7.getWeight() + delta_o2 * w8.getWeight()
+       
+       
+    dw1 = dEtotal_dout_h1 * out_h1 * (1 - out_h1) * i1.getValue()
+    dw2 = dEtotal_dout_h1 * out_h1 * (1 - out_h1) * i2.getValue()
+    dw3 = dEtotal_dout_h2 * out_h2 * (1 - out_h2) * i1.getValue()
+    dw4 = dEtotal_dout_h2 * out_h2 * (1 - out_h2) * i2.getValue()
+    
+    dw5 = delta_o1 * out_h1
+    dw6 = delta_o1 * out_h2
+    dw7 = delta_o2 * out_h1
+    dw8 = delta_o2 * out_h2
+    
+    
+    w1.setWeight(w1.getWeight() - n * dw1)
+    w2.setWeight(w2.getWeight() - n * dw2)
+    w3.setWeight(w3.getWeight() - n * dw3)
+    w4.setWeight(w4.getWeight() - n * dw4)
+    w5.setWeight(w5.getWeight() - n * dw5)
+    w6.setWeight(w6.getWeight() - n * dw6)
+    w7.setWeight(w7.getWeight() - n * dw7)
+    w8.setWeight(w8.getWeight() - n * dw8)
+    
+   
  
 
 if __name__ == '__main__': # pragma: no cover
@@ -103,21 +164,21 @@ if __name__ == '__main__': # pragma: no cover
     b2 = 0.60            
     n = 10.0 # learning speed
    
-    iNeurons = [] 
-    for x in range (0, 5):
+            
+    for x in range (0, 2):
         i = Neuron("i")
         i.setPos(100, 10 + 50*x)
         iNeurons.append(i)
         
-    hNeurons = [] 
-    for x in range (0, 3):
+
+    for x in range (0, 2):
         h = Neuron("h")
         h.setPos(200, 10 + 50*x)
         h.setB(b1)
         hNeurons.append(h)
     
-    oNeurons = [] 
-    for x in range (0, 5):
+
+    for x in range (0, 2):
         o = Neuron("o")
         o.setPos(300, 10 + 50*x)
         o.setB(b2)
@@ -127,13 +188,13 @@ if __name__ == '__main__': # pragma: no cover
     
     for i in iNeurons:
         for h in hNeurons:
-            f = Sinaps("f", random.random() / 3)
+            f = Sinaps("f", random.random() / 2)
             i.addOutSinaps(f)
             h.addInSinaps(f)
     
     for h in hNeurons:
         for o in oNeurons:
-            w = Sinaps("w", random.random() / 3)
+            w = Sinaps("w", random.random() / 2)
             h.addOutSinaps(w)
             o.addInSinaps(w)
      
