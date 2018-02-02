878127d86b6f57f85ab7cb9a3d5a9c9d05be3f53
hukka-mail.ru <hukka@mail.ru>
2018-02-02 16:34:07 +0300

    d

--- src/generic.py
+++ src/generic.py
@@ -14,7 +14,7 @@ import brain
 
 if __name__ == '__main__': # pragma: no cover
 
-    graphics = show.Graphics()
+    graphics = show.Graphics() 
     brain = brain.Brain()
     
     b1 = .35
@@ -33,23 +33,30 @@ if __name__ == '__main__': # pragma: no cover
     for trie in range(1, tries):
    
         brain.clear()
-        brain.create(2, 3, 2)
+        brain.create(2, 4, 2)
            
-                  
+                   
         for x in range(1, 200):
         
-            print("ONE", x)            
-            brain.teach([1,1], [0,1])       
+            print("\nONE", x)            
+            brain.teach([0,1], [0,1])       
             err1 = brain.learn(learningSpeed)
             
             
             print("TWO", x)                        
-            brain.teach([1,0], [1,0])      
+            brain.teach([1,0], [0,1])      
             err2 = brain.learn(learningSpeed)    
              
              
+            print("THREE", x)                        
+            brain.teach([1,1], [1,1])      
+            err3 = brain.learn(learningSpeed)  
+            
+            print("FOUR", x)                        
+            brain.teach([0,0], [0,0])      
+            err4 = brain.learn(learningSpeed)  
              
-            if(err1 < 0.02 and err2 < 0.02):
+            if(err1 < 0.02 and err2 < 0.02 and err3 < 0.02  and err4 < 0.02 ):
                 
                 if(x < minX):
                     minX = x
